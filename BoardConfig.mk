ec4febbacfdab3248ee4cbcf3e8629032860d0d0
Seraph08 <vandend278@cobleskill.edu>
2018-12-20 11:13:32 -0500

    mata: sepolicy file does not exist
    and never will with how AOSPA does their sepol

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,6 @@ BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 VENDOR_SECURITY_PATCH := 2018-12-05
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
